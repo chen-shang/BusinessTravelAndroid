9509af39389c81d27673c0e0b1772991ecff4b54
chenshang <chenshangchenshang@alibaba-inc.com>
2021-10-28 14:42:44 +0800

    MainActivity

--- app/src/main/java/com/business/travel/app/MainActivity.java
+++ app/src/main/java/com/business/travel/app/MainActivity.java
@@ -1,13 +1,31 @@
 package com.business.travel.app;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import java.util.Objects;
 
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import com.blankj.utilcode.util.BarUtils;
+import com.blankj.utilcode.util.ColorUtils;
+
+/**
+ * @author chenshang
+ */
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Objects.requireNonNull(getSupportActionBar()).hide();
+		BarUtils.setStatusBarColor(this, ColorUtils.getColor(R.color.white));
 		setContentView(R.layout.activity_main);
+	}
+
+	public void goMasterActivity(View view) {}
+
+	public void goAccountLongActivity(View view) {}
+
+	public void goSingUpActivity(View view) {
+
 	}
 }
