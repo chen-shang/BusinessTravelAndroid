a971df09ae4090214351550365c156d297385a7f
chenshang <chenshangchenshang@alibaba-inc.com>
2021-11-02 22:50:42 +0800

    项目列表页

--- app/src/main/java/com/business/travel/app/ui/fragment/ProjectFragment.java
+++ app/src/main/java/com/business/travel/app/ui/fragment/ProjectFragment.java
@@ -1,6 +1,5 @@
 package com.business.travel.app.ui.fragment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Bundle;
@@ -10,6 +9,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.business.travel.app.dal.dao.ProjectDao;
+import com.business.travel.app.dal.db.AppDatabase;
 import com.business.travel.app.dal.entity.Project;
 import com.business.travel.app.databinding.FragmentProjectBinding;
 import com.business.travel.app.ui.base.BaseFragment;
@@ -19,21 +20,24 @@ import com.business.travel.app.ui.base.BaseFragment;
  */
 public class ProjectFragment extends BaseFragment<FragmentProjectBinding> {
 
+	private ProjectDao projectDao;
+	private List<Project> projects;
+
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = super.onCreateView(inflater, container, savedInstanceState);
+		projectDao = AppDatabase.getInstance(getContext()).projectDao();
+		projects = projectDao.selectAll();
+		RecyclerView uiRecyclerView = binding.uiProjectFragmentSwipeRecyclerView;
+		uiRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+		ProjectAdapter projectAdapter = new ProjectAdapter(projects);
+		uiRecyclerView.setAdapter(projectAdapter);
 
-		RecyclerView recyclerView = binding.recyclerView;
-		List<Project> list = new ArrayList<>();
-		for (int i = 0; i < 30; i++) {
-			Project project = new Project();
-			project.setName("测试" + i);
-			list.add(project);
-		}
-		recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-		ProjectAdapter projectAdapter = new ProjectAdapter(list);
-		recyclerView.setAdapter(projectAdapter);
-		
+		binding.uiProjectFragmentSwipeRefreshLayout.setOnRefreshListener(() -> {
+			projects.clear();
+			projects.addAll(projectDao.selectAll());
+			binding.uiProjectFragmentSwipeRefreshLayout.setRefreshing(false);
+		});
 		return view;
 	}
 }
