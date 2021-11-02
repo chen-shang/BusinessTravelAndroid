917ee46e23e0028f98eda322fad93f376b858eed
chenshang <chenshangchenshang@alibaba-inc.com>
2021-11-02 22:27:53 +0800

    db dao

--- app/src/main/java/com/business/travel/app/dal/db/AppDatabase.java
+++ app/src/main/java/com/business/travel/app/dal/db/AppDatabase.java
@@ -4,13 +4,24 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import com.business.travel.app.dal.dao.AssociateDao;
+import com.business.travel.app.dal.dao.BillDao;
+import com.business.travel.app.dal.dao.ProjectDao;
 import com.business.travel.app.dal.dao.UserDao;
+import com.business.travel.app.dal.entity.Associate;
+import com.business.travel.app.dal.entity.Bill;
+import com.business.travel.app.dal.entity.Project;
 import com.business.travel.app.dal.entity.User;
 
 /**
  * @author chenshang
  */
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {
+		User.class,
+		Bill.class,
+		Project.class,
+		Associate.class
+}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 	private static AppDatabase INSTANCE;
 
@@ -25,5 +36,11 @@ public abstract class AppDatabase extends RoomDatabase {
 	}
 
 	public abstract UserDao userDao();
+
+	public abstract AssociateDao associateDao();
+
+	public abstract BillDao billDao();
+
+	public abstract ProjectDao projectDao();
 
 }
