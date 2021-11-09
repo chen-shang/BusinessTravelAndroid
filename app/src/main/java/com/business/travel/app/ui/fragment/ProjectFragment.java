978d1c9154e0ca6afe67bed14e61fb4d4fde0883
chenshang <chenshangchenshang@alibaba-inc.com>
2021-11-09 23:13:33 +0800

    adapter

--- app/src/main/java/com/business/travel/app/ui/fragment/ProjectFragment.java
+++ app/src/main/java/com/business/travel/app/ui/fragment/ProjectFragment.java
@@ -2,6 +2,7 @@ package com.business.travel.app.ui.fragment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import com.business.travel.app.constant.ThreadPoolConstant;
 import com.business.travel.app.dal.dao.ProjectDao;
 import com.business.travel.app.dal.db.AppDatabase;
 import com.business.travel.app.dal.entity.Project;
@@ -17,51 +17,52 @@ import com.business.travel.app.databinding.FragmentProjectBinding;
 import com.business.travel.app.ui.MasterActivity;
 import com.business.travel.app.ui.base.BaseFragment;
 import com.business.travel.app.ui.base.ShareData;
+import com.business.travel.app.utils.LogToast;
 
 /**
  * @author chenshang
  */
 public class ProjectFragment extends BaseFragment<FragmentProjectBinding, ShareData> {
 
-	private final List<Project> projects = new ArrayList<>();
-	private ProjectRecyclerViewAdapter projectListRecyclerViewAdapter;
-	private ProjectDao projectDao;
+    private final List<Project> projects = new ArrayList<>();
+    private ProjectRecyclerViewAdapter projectListRecyclerViewAdapter;
+    private ProjectDao projectDao;
 
-	@Override
-	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = super.onCreateView(inflater, container, savedInstanceState);
-		projectDao = AppDatabase.getInstance(getContext()).projectDao();
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        projectDao = AppDatabase.getInstance(getContext()).projectDao();
 
-		viewBinding.UIProjectFragmentSwipeRecyclerViewProjectList.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false));
-		projectListRecyclerViewAdapter = new ProjectRecyclerViewAdapter(projects, (MasterActivity)requireActivity());
-		viewBinding.UIProjectFragmentSwipeRecyclerViewProjectList.setAdapter(projectListRecyclerViewAdapter);
-		viewBinding.UIProjectFragmentSwipeRefreshLayout.setOnRefreshListener(() -> {
-			//下滑刷新项目列表
-			ThreadPoolConstant.COMMON_THREAD_POOL.submit(() -> {
-				refreshProjectList();
-				viewBinding.UIProjectFragmentSwipeRefreshLayout.setRefreshing(false);
-			});
-		});
-		return view;
-	}
+        viewBinding.UIProjectFragmentSwipeRecyclerViewProjectList.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false));
+        projectListRecyclerViewAdapter = new ProjectRecyclerViewAdapter(projects, (MasterActivity)requireActivity());
+        viewBinding.UIProjectFragmentSwipeRecyclerViewProjectList.setAdapter(projectListRecyclerViewAdapter);
+        viewBinding.UIProjectFragmentSwipeRefreshLayout.setOnRefreshListener(() -> {
+            //下滑刷新项目列表
+            CompletableFuture.runAsync(this::refreshProjectList).whenComplete((unused, throwable) -> {
+                LogToast.infoShow("更新成功");
+                viewBinding.UIProjectFragmentSwipeRefreshLayout.setRefreshing(false);
+            });
+        });
+        return view;
+    }
 
-	/**
-	 * 刷新项目列表
-	 * TODO: 后续改成分页
-	 */
-	private void refreshProjectList() {
-		//先清空
-		projects.clear();
-		//在查询全量
-		projects.addAll(projectDao.selectAll());
-		//通知adapter更新列表
-		projectListRecyclerViewAdapter.notifyDataSetChanged();
-	}
+    /**
+     * 刷新项目列表
+     * TODO: 后续改成分页
+     */
+    private void refreshProjectList() {
+        //先清空
+        projects.clear();
+        //在查询全量
+        projects.addAll(projectDao.selectAll());
+        //通知adapter更新列表
+        projectListRecyclerViewAdapter.notifyDataSetChanged();
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		// 每次进来的时候,都要刷新一下项目列表
-		refreshProjectList();
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        // 每次进来的时候,都要刷新一下项目列表
+        refreshProjectList();
+    }
 }
