40118e3c3b66986dcdc3c9303061a98dfac2c6ec
chenshang <chenshangchenshang@alibaba-inc.com>
2021-11-09 22:47:12 +0800

    Future

---
+++ app/src/main/java/com/business/travel/app/utils/Future.java
@@ -0,0 +1,20 @@
+package com.business.travel.app.utils;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+import com.business.travel.app.constant.ThreadPoolConstant;
+
+/**
+ * @author chenshang
+ */
+public class Future {
+	public static CompletableFuture<Void> runAsync(Runnable runnable) {
+		return CompletableFuture.runAsync(runnable, ThreadPoolConstant.COMMON_THREAD_POOL);
+	}
+
+	public static <U> CompletableFuture<U> supplyAsync(Supplier<U> supplier) {
+		return CompletableFuture.supplyAsync(supplier, ThreadPoolConstant.COMMON_THREAD_POOL);
+	}
+
+}
