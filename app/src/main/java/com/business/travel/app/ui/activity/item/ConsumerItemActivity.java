a554fc879db07b919071f77dfcbe349a4b8b4bb8
chenshang <chenshangchenshang@alibaba-inc.com>
2021-11-11 19:34:17 +0800

    new activity

---
+++ app/src/main/java/com/business/travel/app/ui/activity/item/ConsumerItemActivity.java
@@ -0,0 +1,16 @@
+package com.business.travel.app.ui.activity.item;
+
+import android.os.Bundle;
+import com.business.travel.app.databinding.ActivityConsumerItemBinding;
+import com.business.travel.app.ui.base.BaseActivity;
+
+/**
+ * @author chenshang
+ */
+public class ConsumerItemActivity extends BaseActivity<ActivityConsumerItemBinding> {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
