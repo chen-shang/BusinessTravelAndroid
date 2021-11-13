b088aa6b43d1e1826b55d72937e813f72a19ee46
chenshang <chenshangchenshang@alibaba-inc.com>
2021-11-13 12:34:18 +0800

    消费项名称

--- app/src/main/java/com/business/travel/app/dal/entity/ConsumerItem.java
+++ app/src/main/java/com/business/travel/app/dal/entity/ConsumerItem.java
@@ -1,7 +1,6 @@
 package com.business.travel.app.dal.entity;
 
 import androidx.room.Entity;
-import androidx.room.Index;
 import androidx.room.PrimaryKey;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,25 +8,29 @@ import lombok.EqualsAndHashCode;
 /**
  * @author chenshang
  */
-@Entity(indices = {@Index(value = {"name", "icon", "type"}, unique = true)})
+@Entity
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class ConsumerItem extends BaseEntity {
-    /**
-     * 主键id
-     */
-    @PrimaryKey(autoGenerate = true)
-    private Long id;
-    /**
-     * 消费项名称
-     */
-    private String name;
-    /**
-     * 消费项图标
-     */
-    private String icon;
-    /**
-     * 消费项目类型 0:支出 1:收入
-     */
-    private Integer type;
+	/**
+	 * 主键id
+	 */
+	@PrimaryKey(autoGenerate = true)
+	private Long id;
+	/**
+	 * 消费项名称
+	 */
+	private String name;
+	/**
+	 * 消费项图标大类
+	 */
+	private String iconPath;
+	/**
+	 * 消费项图标名称
+	 */
+	private String iconName;
+	/**
+	 * 消费项目类型 0:支出 1:收入
+	 */
+	private Integer type;
 }
