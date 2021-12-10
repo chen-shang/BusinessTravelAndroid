e409a6caffc4cf59a4ce862e7df5de7018ce6b63
chenshang <chenshangchenshang@alibaba-inc.com>
2021-12-10 11:17:09 +0800

    ReportingPolicy.IGNORE,

--- app/src/main/java/com/business/travel/app/model/converter/ConsumptionConverter.java
+++ app/src/main/java/com/business/travel/app/model/converter/ConsumptionConverter.java
@@ -3,9 +3,10 @@ package com.business.travel.app.model.converter;
 import com.business.travel.app.dal.entity.Consumption;
 import com.business.travel.app.model.ImageIconInfo;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(unmappedSourcePolicy = ReportingPolicy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ConsumptionConverter {
 	ConsumptionConverter INSTANCE = Mappers.getMapper(ConsumptionConverter.class);
 
--- app/src/main/java/com/business/travel/app/model/converter/MemberConverter.java
+++ app/src/main/java/com/business/travel/app/model/converter/MemberConverter.java
@@ -3,9 +3,10 @@ package com.business.travel.app.model.converter;
 import com.business.travel.app.dal.entity.Member;
 import com.business.travel.app.model.ImageIconInfo;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(unmappedSourcePolicy = ReportingPolicy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberConverter {
 	MemberConverter INSTANCE = Mappers.getMapper(MemberConverter.class);
 
