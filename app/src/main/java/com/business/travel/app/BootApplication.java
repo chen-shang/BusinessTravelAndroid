783b6458d6b1211b496c3843232eaebe89647acc
chenshang <chenshangchenshang@alibaba-inc.com>
2021-12-14 10:58:25 +0800

    升级jar

--- app/src/main/java/com/business/travel/app/BootApplication.java
+++ app/src/main/java/com/business/travel/app/BootApplication.java
@@ -1,6 +1,7 @@
 package com.business.travel.app;
 
 import android.app.Application;
+import com.blankj.utilcode.util.CrashUtils;
 import com.blankj.utilcode.util.LogUtils;
 
 public class BootApplication extends Application {
@@ -9,5 +10,7 @@ public class BootApplication extends Application {
 		super.onCreate();
 		//开启日志记录写入文件,最大栈深度为5,最多保留一天 todo 生产环境关闭日志提高程序速度
 		LogUtils.getConfig().setLog2FileSwitch(true).setStackDeep(5).setSaveDays(1).setLogSwitch(true);
+
+		CrashUtils.init();
 	}
 }
