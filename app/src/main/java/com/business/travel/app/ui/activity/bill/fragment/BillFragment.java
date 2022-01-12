cbadfdbbc9ca7a6bfac8beb5aa0b6cf2ca0dc974
chenshang <chenshangchenshang@alibaba-inc.com>
2022-01-12 13:55:43 +0800

    fix

--- app/src/main/java/com/business/travel/app/ui/activity/bill/fragment/BillFragment.java
+++ app/src/main/java/com/business/travel/app/ui/activity/bill/fragment/BillFragment.java
@@ -102,8 +102,6 @@ public class BillFragment extends BaseFragment<FragmentBillBinding> {
 	@Override
 	public void onResume() {
 		super.onResume();
-		floatingActionButton.setImageResource(R.drawable.ic_base_bill_add);
-
 		int lastFragment = masterActivity.getLastFragment();
 		int currentFragment = masterActivity.getCurrentFragment();
 		//旋转上升动画显示中间的加号
@@ -126,8 +124,6 @@ public class BillFragment extends BaseFragment<FragmentBillBinding> {
 		} else {
 			AnimalUtil.reset(floatingActionButton, Orientation.RIGHT_LEFT);
 		}
-
-		floatingActionButton.setImageResource(R.drawable.ic_base_bill_add_gray);
 	}
 
 	/**
--- app/src/main/java/com/business/travel/app/utils/AnimalUtil.java
+++ app/src/main/java/com/business/travel/app/utils/AnimalUtil.java
@@ -1,10 +1,13 @@
 package com.business.travel.app.utils;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.graphics.drawable.GradientDrawable.Orientation;
-import android.view.View;
 import android.view.animation.DecelerateInterpolator;
+import com.business.travel.app.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * @author chenshang
@@ -16,20 +19,27 @@ public class AnimalUtil {
 	/**
 	 * 旋转上升动画
 	 *
-	 * @param view
+	 * @param floatingActionButton
 	 */
-	public static void show(View view, Orientation orientation) {
+	public static void show(FloatingActionButton floatingActionButton, Orientation orientation) {
 		AnimatorSet animatorSet = new AnimatorSet();
-		ObjectAnimator rotation = genRotation(view, orientation);
-		ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", 0.8f, 1.1f);
-		ObjectAnimator scaleY = ObjectAnimator.ofFloat(view, "scaleY", 0.8f, 1.1f);
-		ObjectAnimator translationY = ObjectAnimator.ofFloat(view, "translationY", 0, -view.getHeight() * 0.45F);
+		ObjectAnimator rotation = genRotation(floatingActionButton, orientation);
+		ObjectAnimator scaleX = ObjectAnimator.ofFloat(floatingActionButton, "scaleX", 0.8f, 1.1f);
+		ObjectAnimator scaleY = ObjectAnimator.ofFloat(floatingActionButton, "scaleY", 0.8f, 1.1f);
+		ObjectAnimator translationY = ObjectAnimator.ofFloat(floatingActionButton, "translationY", 0, -floatingActionButton.getHeight() * 0.45F);
 		//动画时间
 		animatorSet.setDuration(duration);
 		//设置插值器
 		animatorSet.setInterpolator(new DecelerateInterpolator());
 		//同时执行
 		animatorSet.play(rotation).with(scaleX).with(scaleY).with(translationY);
+		animatorSet.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationStart(Animator animation) {
+				super.onAnimationResume(animation);
+				floatingActionButton.setImageResource(R.drawable.ic_base_bill_add);
+			}
+		});
 		//启动动画
 		animatorSet.start();
 	}
@@ -37,31 +47,37 @@ public class AnimalUtil {
 	/**
 	 * 旋转复位动画
 	 *
-	 * @param view
+	 * @param floatingActionButton
 	 */
-	public static void reset(View view, Orientation orientation) {
+	public static void reset(FloatingActionButton floatingActionButton, Orientation orientation) {
 		AnimatorSet animatorSet = new AnimatorSet();
-		ObjectAnimator rotation = genRotation(view, orientation);
-		ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", 1.1f, 0.8f);
-		ObjectAnimator scaleY = ObjectAnimator.ofFloat(view, "scaleY", 1.1f, 0.8f);
-		ObjectAnimator translationY = ObjectAnimator.ofFloat(view, "translationY", -view.getHeight() * 0.45F, 0);
-
+		ObjectAnimator rotation = genRotation(floatingActionButton, orientation);
+		ObjectAnimator scaleX = ObjectAnimator.ofFloat(floatingActionButton, "scaleX", 1.1f, 0.8f);
+		ObjectAnimator scaleY = ObjectAnimator.ofFloat(floatingActionButton, "scaleY", 1.1f, 0.8f);
+		ObjectAnimator translationY = ObjectAnimator.ofFloat(floatingActionButton, "translationY", -floatingActionButton.getHeight() * 0.45F, 0);
 		//动画时间
 		animatorSet.setDuration(duration);
 		//设置插值器
 		animatorSet.setInterpolator(new DecelerateInterpolator());
 		//同时执行
 		animatorSet.play(rotation).with(scaleX).with(scaleY).with(translationY);
+		animatorSet.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				super.onAnimationEnd(animation);
+				floatingActionButton.setImageResource(R.drawable.ic_base_bill_add_gray);
+			}
+		});
 		//启动动画
 		animatorSet.start();
 	}
 
-	private static ObjectAnimator genRotation(View view, Orientation orientation) {
+	private static ObjectAnimator genRotation(FloatingActionButton floatingActionButton, Orientation orientation) {
 		if (Orientation.RIGHT_LEFT == orientation) {
-			return ObjectAnimator.ofFloat(view, "rotation", 0.0F, v);
+			return ObjectAnimator.ofFloat(floatingActionButton, "rotation", 0.0F, v);
 		} else if (Orientation.LEFT_RIGHT == orientation) {
-			return ObjectAnimator.ofFloat(view, "rotation", v, 0.0F);
+			return ObjectAnimator.ofFloat(floatingActionButton, "rotation", v, 0.0F);
 		}
-		return ObjectAnimator.ofFloat(view, "rotation", 0.0F, v);
+		return ObjectAnimator.ofFloat(floatingActionButton, "rotation", 0.0F, v);
 	}
 }
