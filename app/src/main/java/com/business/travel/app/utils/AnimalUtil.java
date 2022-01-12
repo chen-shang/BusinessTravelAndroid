f9662aeba8c4afaeb97709af3d995ba3fbc7593d
chenshang <chenshangchenshang@alibaba-inc.com>
2022-01-12 13:58:25 +0800

    fix

--- app/src/main/java/com/business/travel/app/utils/AnimalUtil.java
+++ app/src/main/java/com/business/travel/app/utils/AnimalUtil.java
@@ -13,8 +13,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
  * @author chenshang
  */
 public class AnimalUtil {
-	private static final float v = 180.0F;
+	/**
+	 * 旋转角度
+	 */
+	private static final float rotation = 180.0F;
+	/**
+	 * 持续事件
+	 */
 	private static final long duration = 800;
+	/**
+	 * 开始大小
+	 */
+	private static final float startSize = 0.6f;
+
+	/**
+	 * 结束大小
+	 */
+	private static final float endSize = 1.1f;
 
 	/**
 	 * 旋转上升动画
@@ -24,8 +39,8 @@ public class AnimalUtil {
 	public static void show(FloatingActionButton floatingActionButton, Orientation orientation) {
 		AnimatorSet animatorSet = new AnimatorSet();
 		ObjectAnimator rotation = genRotation(floatingActionButton, orientation);
-		ObjectAnimator scaleX = ObjectAnimator.ofFloat(floatingActionButton, "scaleX", 0.8f, 1.1f);
-		ObjectAnimator scaleY = ObjectAnimator.ofFloat(floatingActionButton, "scaleY", 0.8f, 1.1f);
+		ObjectAnimator scaleX = ObjectAnimator.ofFloat(floatingActionButton, "scaleX", startSize, endSize);
+		ObjectAnimator scaleY = ObjectAnimator.ofFloat(floatingActionButton, "scaleY", startSize, endSize);
 		ObjectAnimator translationY = ObjectAnimator.ofFloat(floatingActionButton, "translationY", 0, -floatingActionButton.getHeight() * 0.45F);
 		//动画时间
 		animatorSet.setDuration(duration);
@@ -52,8 +67,8 @@ public class AnimalUtil {
 	public static void reset(FloatingActionButton floatingActionButton, Orientation orientation) {
 		AnimatorSet animatorSet = new AnimatorSet();
 		ObjectAnimator rotation = genRotation(floatingActionButton, orientation);
-		ObjectAnimator scaleX = ObjectAnimator.ofFloat(floatingActionButton, "scaleX", 1.1f, 0.8f);
-		ObjectAnimator scaleY = ObjectAnimator.ofFloat(floatingActionButton, "scaleY", 1.1f, 0.8f);
+		ObjectAnimator scaleX = ObjectAnimator.ofFloat(floatingActionButton, "scaleX", endSize, startSize);
+		ObjectAnimator scaleY = ObjectAnimator.ofFloat(floatingActionButton, "scaleY", endSize, startSize);
 		ObjectAnimator translationY = ObjectAnimator.ofFloat(floatingActionButton, "translationY", -floatingActionButton.getHeight() * 0.45F, 0);
 		//动画时间
 		animatorSet.setDuration(duration);
@@ -74,10 +89,10 @@ public class AnimalUtil {
 
 	private static ObjectAnimator genRotation(FloatingActionButton floatingActionButton, Orientation orientation) {
 		if (Orientation.RIGHT_LEFT == orientation) {
-			return ObjectAnimator.ofFloat(floatingActionButton, "rotation", 0.0F, v);
+			return ObjectAnimator.ofFloat(floatingActionButton, "rotation", 0.0F, rotation);
 		} else if (Orientation.LEFT_RIGHT == orientation) {
-			return ObjectAnimator.ofFloat(floatingActionButton, "rotation", v, 0.0F);
+			return ObjectAnimator.ofFloat(floatingActionButton, "rotation", rotation, 0.0F);
 		}
-		return ObjectAnimator.ofFloat(floatingActionButton, "rotation", 0.0F, v);
+		return ObjectAnimator.ofFloat(floatingActionButton, "rotation", 0.0F, rotation);
 	}
 }
