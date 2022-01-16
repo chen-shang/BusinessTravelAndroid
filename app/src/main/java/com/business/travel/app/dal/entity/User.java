0ec8cd830cfa67769d879cac075899513099bdf0
chenshang <chenshang@chenshangdeMacBook-Pro.local>
2022-01-16 23:40:31 +0800

    同意隐私协议和用户条款 添加字段

--- app/src/main/java/com/business/travel/app/dal/entity/User.java
+++ app/src/main/java/com/business/travel/app/dal/entity/User.java
@@ -13,26 +13,30 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class User extends BaseEntity {
-	@PrimaryKey(autoGenerate = true)
-	private Long id;
-	/**
-	 * 头像地址 可以是网络剧也可以是本地
-	 */
-	private String icon;
-	/**
-	 * 唯一编码，不同的用户有不同的编码
-	 */
-	private String code;
-	/**
-	 * 用户名字,逗号分隔
-	 */
-	private String name;
-	/**
-	 * 手机号
-	 */
-	private String phone;
-	/**
-	 * 年龄
-	 */
-	private String age;
+    @PrimaryKey(autoGenerate = true)
+    private Long id;
+    /**
+     * 头像地址 可以是网络剧也可以是本地
+     */
+    private String icon;
+    /**
+     * 唯一编码，不同的用户有不同的编码
+     */
+    private String code;
+    /**
+     * 用户名字,逗号分隔
+     */
+    private String name;
+    /**
+     * 手机号
+     */
+    private String phone;
+    /**
+     * 年龄
+     */
+    private String age;
+    /**
+     * 同意隐私协议和用户条款
+     */
+    private Boolean agree;
 }
