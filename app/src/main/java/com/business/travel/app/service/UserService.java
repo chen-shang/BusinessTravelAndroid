0369b3e87fed9308318a195a2690cbe5111158e3
chenshang <chenshang@chenshangdeMacBook-Pro.local>
2022-01-16 23:51:29 +0800

    service 实现查询用户函数

--- app/src/main/java/com/business/travel/app/service/UserService.java
+++ app/src/main/java/com/business/travel/app/service/UserService.java
@@ -3,6 +3,7 @@ package com.business.travel.app.service;
 import android.content.Context;
 import com.business.travel.app.dal.dao.UserDao;
 import com.business.travel.app.dal.db.AppDatabase;
+import com.business.travel.app.dal.entity.User;
 
 /**
  * 用户相关的业务逻辑
@@ -13,5 +14,14 @@ public class UserService {
 
     public UserService(Context context) {
         userDao = AppDatabase.getInstance(context).userDao();
+    }
+
+    /**
+     * 查询当前用户信息
+     *
+     * @return
+     */
+    public User queryUser() {
+        return userDao.selectOne();
     }
 }
