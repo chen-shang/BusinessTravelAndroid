d68566c144503aaf10f1c82b3ade85d1a7c1901e
chenshang <chenshang@chenshangdeMacBook-Pro.local>
2022-01-16 23:43:50 +0800

    UserDao 添加 实现 selectAll、selectOne

--- app/src/main/java/com/business/travel/app/dal/dao/UserDao.java
+++ app/src/main/java/com/business/travel/app/dal/dao/UserDao.java
@@ -1,12 +1,27 @@
 package com.business.travel.app.dal.dao;
 
 import androidx.room.Dao;
+import androidx.room.Query;
 import com.business.travel.app.dal.dao.base.BaseDao;
-import com.business.travel.app.dal.entity.Project;
+import com.business.travel.app.dal.entity.User;
+
+import java.util.List;
 
 /**
  * @author chenshang
  */
 @Dao
-public interface UserDao extends BaseDao<Project> {
+public interface UserDao extends BaseDao<User> {
+
+    /**
+     * 查询全部
+     */
+    @Query("SELECT * FROM User where isDeleted=1")
+    List<User> selectAll();
+
+    /**
+     * 查询全部
+     */
+    @Query("SELECT * FROM User where isDeleted=1 limit 1")
+    User selectOne();
 }
