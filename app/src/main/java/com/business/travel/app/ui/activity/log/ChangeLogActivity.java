0aed6bda7d01471dfd83cc3d4dba50f9cf4efd36
chenshang <chenshang@chenshangdeMacBook-Pro.local>
2022-01-17 23:29:58 +0800

    实时更新图标按钮和切换是否同意用户协议按钮

--- app/src/main/java/com/business/travel/app/ui/activity/log/ChangeLogActivity.java
+++ app/src/main/java/com/business/travel/app/ui/activity/log/ChangeLogActivity.java
@@ -1,42 +1,66 @@
 package com.business.travel.app.ui.activity.log;
 
-import java.io.File;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import com.blankj.utilcode.util.FileIOUtils;
 import com.blankj.utilcode.util.LogUtils;
 import com.business.travel.app.R;
+import com.business.travel.app.constant.AppConfig;
+import com.business.travel.app.dal.entity.User;
 import com.business.travel.app.databinding.ActivityChangeLogBinding;
+import com.business.travel.app.service.UserService;
 import com.business.travel.app.ui.base.BaseActivity;
+import com.business.travel.app.utils.LogToast;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChangeLogActivity extends BaseActivity<ActivityChangeLogBinding> {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		viewBinding.log.setMovementMethod(ScrollingMovementMethod.getInstance());
-		viewBinding.showLog.setOnClickListener(v -> {
-			final List<File> logFiles = LogUtils.getLogFiles();
-			final String log = logFiles.stream().map(FileIOUtils::readFile2String).collect(Collectors.joining("\n"));
-			viewBinding.log.setText(log);
-		});
+    private UserService userService;
 
-		viewBinding.clearLog.setOnClickListener(v -> {
-			final List<File> logFiles = LogUtils.getLogFiles();
-			logFiles.forEach(File::delete);
-		});
-	}
+    @Override
+    protected void inject() {
+        super.inject();
+        userService = new UserService(this);
+    }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewBinding.log.setMovementMethod(ScrollingMovementMethod.getInstance());
+        viewBinding.showLog.setOnClickListener(v -> {
+            final List<File> logFiles = LogUtils.getLogFiles();
+            final String log = logFiles.stream().map(FileIOUtils::readFile2String).collect(Collectors.joining("\n"));
+            viewBinding.log.setText(log);
+        });
 
-		//加载 change log
-		String[] changeLog = this.getResources().getStringArray(R.array.change_log);
-		final String collect = String.join("\n", changeLog);
-		viewBinding.versionHistory.setText(collect);
-	}
+        viewBinding.clearLog.setOnClickListener(v -> {
+            final List<File> logFiles = LogUtils.getLogFiles();
+            logFiles.forEach(File::delete);
+        });
+
+        viewBinding.clearCache.setOnClickListener(v -> {
+            LogToast.infoShow("实时更新图标");
+            AppConfig.getConfig().setIconTtl(0L);
+        });
+
+        viewBinding.changeAgree.setOnClickListener(v -> {
+            User user = userService.queryUser();
+            user.setAgree(!user.getAgree());
+            userService.upsertUser(user);
+            LogToast.infoShow("同意用户协议:" + user.getAgree());
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //加载 change log
+        String[] changeLog = this.getResources().getStringArray(R.array.change_log);
+        final String collect = String.join("\n", changeLog);
+        viewBinding.versionHistory.setText(collect);
+    }
 }
