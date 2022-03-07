10c1951ef349da4a83b621d416031450d9e4eb8a
chenshang <chenshangchenshang@alibaba-inc.com>
2022-03-07 13:27:54 +0800

    签名

--- app/src/main/java/com/business/travel/app/MainActivity.java
+++ app/src/main/java/com/business/travel/app/MainActivity.java
@@ -114,7 +114,7 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
          */
         UMConfigure.init(this, "61eeb912e014255fcb04447f", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
 
-        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+        PlatformConfig.setWeixin("wxaae6cbabcbc0aa53", "004b5c1fdfffae940090d8ab4f8b3d3f");
 
         //要充分利用启动页面的停顿时间,尽量做一些后台工作,比如检查网络,同步数据之类的,初始化数据之类
         //因为这个类只在启动的时候启动一次,不会重复做一些事情,可以提升其他页面的访问速度
--- app/src/main/java/com/business/travel/app/ui/activity/login/LoginActivity.java
+++ app/src/main/java/com/business/travel/app/ui/activity/login/LoginActivity.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.view.View;
 import com.business.travel.app.databinding.ActivityLoginBinding;
 import com.business.travel.app.ui.base.BaseActivity;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.PlatformConfig;
 
 public class LoginActivity extends BaseActivity<ActivityLoginBinding> {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		/**
+		 * 注意: 即使您已经在AndroidManifest.xml中配置过appkey和channel值，也需要在App代码中调
+		 * 用初始化接口（如需要使用AndroidManifest.xml中配置好的appkey和channel值，
+		 * UMConfigure.init调用中appkey和channel参数请置为null）。
+		 */
+		UMConfigure.init(this, "61eeb912e014255fcb04447f", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
+
+		PlatformConfig.setWeixin("wxaae6cbabcbc0aa53", "004b5c1fdfffae940090d8ab4f8b3d3f");
 	}
 
 	public void goMasterActivity(View view) {
